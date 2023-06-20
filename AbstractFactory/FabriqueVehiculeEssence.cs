25907381664f1f060bd695a3417926d528a67148
Estrems <74878108+MegaCretin@users.noreply.github.com>
2023-06-20 12:53:12 +0200

    Correction BuilderFactory

--- AbstractFactory/FabriqueVehiculeEssence.cs
+++ AbstractFactory/FabriqueVehiculeEssence.cs
@@ -2,15 +2,12 @@ using System;
 
 public class FabriqueVehiculeEssence : FabriqueVehicule
 {
-  public Automobile creeAutomobile(string modele, string
-    couleur, int puissance, double espace)
+  public Automobile creeAutomobile(string modele, string couleur, int puissance, double espace)
   {
-    return new AutomobileEssence(modele, couleur,
-      puissance, espace);
+    return new AutomobileEssence(modele, couleur, puissance, espace);
   }
 
-  public Scooter creeScooter(string modele, string
-    couleur, int puissance)
+  public Scooter creeScooter(string modele, string couleur, int puissance)
   {
     return new ScooterEssence(modele, couleur, puissance);
   }
---
+++ BuilderFactory/ConstructeurLiasseVehicule.cs
@@ -0,0 +1,15 @@
+using System;
+
+public abstract class ConstructeurLiasseVehicule
+{
+  protected Liasse liasse;
+
+  public abstract void construitBonDeCommande(string nomClient);
+
+  public abstract void construitDemandeImmatriculation(string nomDemandeur);
+
+  public Liasse resultat()
+  {
+    return liasse;
+  }
+}
